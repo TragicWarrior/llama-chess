d6c654e431601f42f7e45771af2e3e9fea0ceb9e
bjk <bjk>
2002-12-10 22:14:18 +0000

    New message() function handles multiline messages.

--- src/message.c
+++ src/message.c
@@ -1,4 +1,4 @@
-/* $Id: message.c,v 1.1.1.1 2002-12-05 20:38:47 bjk Exp $ */
+/* $Id: message.c,v 1.2 2002-12-10 22:14:18 bjk Exp $ */
 /*
     Copyright (C) 2002 Ben Kibbey <bjk@arbornet.org>
 
@@ -20,25 +20,24 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
-#include <panel.h>
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
 
 #include "common.h"
-
-#define CALCWIDTH(str)	(strlen(str) + 4)
-#define CALCHEIGHT()	(4)
+#include "message.h"
 
 int message(const char *title, const char *prompt, const char *format, ...)
 {
-    va_list ap;
-    char *line, *s, *tmp, buf[LINE_MAX];
     WINDOW *win;
-    PANEL *p;
-    int n;
-    int x, y, c = 0, i = 0;
+    PANEL *panel;
+    FIELD *fields[2];
+    FORM *form;
+    va_list ap;
+    char *line;
+    int width, height;
+    int c;
 
     va_start(ap, format);
 #ifdef HAVE_VASPRINTF
@@ -49,75 +48,49 @@ int message(const char *title, const char *prompt, const char *format, ...)
 #endif
     va_end(ap);
 
-    y = CALCHEIGHT();
-    y += (title)? 1 : 0;
-    tmp = strdup(line);
-    strcpy(buf, line);
+    width = (strlen(line) < MSG_WIDTH) ? strlen(line) : MSG_WIDTH;
+    height = (width < MSG_WIDTH) ? 1 : strlen(line) / MSG_WIDTH + 1;
 
-    while ((s = strsep(&tmp, "\n")) != NULL) {
-	int n = strlen(s);
+    fields[0] = new_field(height, width, 0, 0, 0, 0);
+    set_field_buffer(fields[0], 0, line);
+    set_field_just(fields[0], JUSTIFY_CENTER);
+    field_opts_off(fields[0], O_EDIT);
+    fields[1] = NULL;
+    form = new_form(fields);
+    scale_form(form, &height, &width);
 
-	y++;
+    if (width < strlen(prompt))
+	width = strlen(prompt);
 
-	if (n > i) {
-	    i = n;
-	    strcpy(buf, s);
-	}
-    }
+    if (title && width < strlen(title))
+	width = strlen(title);
 
-    tmp = line;
+    width += 2;
 
-    if (prompt && strlen(prompt) > i)
-	x = CALCWIDTH(prompt);
-    else
-	x = CALCWIDTH(buf);
+    win = newwin((title) ? height + 5 : height + 4, width + 2,
+	    CALCPOSY(((title) ? height + 5 : height + 4)),
+	    CALCPOSX(width));
 
-    if (title && x < strlen(title))
-	x = CALCWIDTH(title);
+    draw_window_title(win, title, width + 2);
 
-    if (!prompt)
-	y--;
+    panel = new_panel(win);
+    set_form_win(form, win);
+    set_form_sub(form, derwin(win, height, width, (title) ? 2 : 1, 1));
+    mvwprintw(win, (title) ? height + 3 : height + 2, 
+	    CENTERX(width, prompt), "%s", prompt);
+    post_form(form);
 
-    if (!title)
-	y--;
-
-    cbreak();
-    noecho();
-
-    win = newwin(y, x, CALCPOSY(y), CALCPOSX(x));
-    p = new_panel(win);
-
-    wattron(win, MESSAGE_CP);
-
-    for (i = 0; i < y; i++) {
-	for (n = 0; n < x; n++)
-	    mvwprintw(win, i, n, " ");
-    }
-
-    box(win, ACS_VLINE, ACS_HLINE);
-
-    if (title) {
-	draw_window_title(win, title, x);
-    }
-
-    i = (title) ? 2 : 1;
-
-    while ((s = strsep(&tmp, "\n")) != NULL)
-	mvwprintw(win, i++, CENTERX(x, s), "%s", s);
-
-    free(tmp);
-    free(line);
-
-    if (prompt)
-	mvwprintw(win, y - 2, CENTERX(x, prompt), "%s", prompt);
-
-    wattroff(win, MESSAGE_CP);
     update_panels();
     doupdate();
 
     c = wgetch(win);
-    del_panel(p);
+
+    unpost_form(form);
+    free_form(form);
+    free_field(fields[0]);
+    del_panel(panel);
     delwin(win);
+    free(line);
 
     return c;
 }
