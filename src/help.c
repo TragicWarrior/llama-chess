1c714e24ed70470894c4e6d3faae325b5fef8cf6
Ben Kibbey <bjk@luxsci.net>
2006-03-16 11:20:14 -0500

    Update my email address.

--- src/help.c
+++ src/help.c
@@ -1,6 +1,6 @@
 /* vim:tw=78:ts=8:sw=4:set ft=c:  */
 /* 
-    Copyright (C) 2002-2006 Ben Kibbey <bjk@arbornet.org>
+    Copyright (C) 2002-2006 Ben Kibbey <bjk@luxsci.net>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
