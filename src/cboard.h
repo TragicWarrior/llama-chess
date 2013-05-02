e939a384c4317300b939ef129a288da1597817f4
Ben Kibbey <bjk@luxsci.net>
2013-05-01 22:48:37 -0400

    Update copyright year.

--- src/cboard.h
+++ src/cboard.h
@@ -1,6 +1,6 @@
 /* vim:tw=78:ts=8:sw=4:set ft=c:  */
 /*
-    Copyright (C) 2002-2011 Ben Kibbey <bjk@luxsci.net>
+    Copyright (C) 2002-2013 Ben Kibbey <bjk@luxsci.net>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 #ifndef CBOARD_H
 #define CBOARD_H
 
-#define COPYRIGHT	"Copyright (C) 2002-2011 " PACKAGE_BUGREPORT
+#define COPYRIGHT	"Copyright (C) 2002-2013 " PACKAGE_BUGREPORT
 #define LINE_GRAPHIC(c)	((!config.linegraphics) ? ' ' : c)
 #define ROWTOMATRIX(r)	((8 - r) * 2 + 2 - 1)
 #define COLTOMATRIX(c)	((c == 1) ? 1 : c * 4 - 3)
