TragicWarrior/llama-chess

commit 0fb5f2e18b3b1435d211468433e81df3f250d126

Fix Coverity issue #101998.

libchess/pgn.c
@@ -1423,7 +1423,10 @@ other:
 	if (!VALIDROW(*tmp))
 	    return E_PGN_PARSE;
 
-	row = 8 - atoi(tmp++);
+        if (!VALIDRANK(*tmp))
+          return E_PGN_ERR;
+
+	row = RANKTOBOARD(*tmp++);
 	b[row][col].enpassant = 1;
 	SET_FLAG(*flags, GF_ENPASSANT);
     }

commit 6e7449ce53b6c17c0486f4b23a71ebf0de078b43

Fix showing the enpassant square.
Broken since commit 0fb5f2e.

libchess/pgn.c
@@ -1543,16 +1543,12 @@ other:
     {
       if (!VALIDCOL (*tmp))
 	return E_PGN_PARSE;
-
       col = *tmp++ - 'a';
 
       if (!VALIDROW (*tmp))
 	return E_PGN_PARSE;
+      row = 8 - atoi (tmp++);
 
-      if (!VALIDRANK (*tmp))
-	return E_PGN_ERR;
-
-      row = RANKTOBOARD (*tmp++);
       b[row][col].enpassant = 1;
       SET_FLAG (*flags, GF_ENPASSANT);
     }